3f37c20ce5d9421bde6180155b66899ccc27b1bd
Paul Thomas <paulthomas2107@gmail.com>
2022-11-16 11:32:44 +0000

    More....

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
-import express, {Express, Request, Response} from "express"
-import dotenv from "dotenv"
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
 
-const app:Express = express()
-const port = process.env.PORT || 3000
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get('/api/jobs', (req: Request, res: Response) => {
+  res.json({ msg: "Hi Paul, it's get jobs..." });
+});
+
+app.post('/api/jobs', (req: Request, res: Response) => {
+  res.json({ msg: "Hi Paul, it's post jobs..." });
+});
+
+app.put('/api/jobs/:id', (req: Request, res: Response) => {
+    res.json({ msg: "Hi Paul, it's put jobs..." });
+  });
+
+  app.delete('/api/jobs/:id', (req: Request, res: Response) => {
+    res.json({ msg: "Hi Paul, it's delete jobs..." });
+  });
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`)
-})
+  console.log(`Server started on port ${port}`);
+});
