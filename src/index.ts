14430a1a3c3265869aa6fa237709b6946e8beb83
Paul Thomas <paulthomas2107@gmail.com>
2022-11-16 10:14:30 +0000

    More....

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello Paul')
+import express, {Express, Request, Response} from "express"
+import dotenv from "dotenv"
+
+const app:Express = express()
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+})
